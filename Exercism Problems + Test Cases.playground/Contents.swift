8f1e35fab7ac9205f6ddc6c7790394541c742bf5
mohammed <mdrame113@gmail.com>
2020-04-20 17:51:31 -0700

    Exercism question one

---
+++ Exercism Problems + Test Cases.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Foundation
+
+//Write a function that accepts a String as its only parameter, and returns true if the string has
+//only unique letters, taking letter case into account.
+
+func isUnique(input string: String)-> Bool  {
+    // 1. create a array of checked characters, then loop throught everry letter in the input string and append the latter of the list of checked characters, returning false as soon as a call to contain () fails.
+    var usedWord = [Character]()
+    
+    for letter in string {
+        if usedWord.contains(letter) {
+            return false
+        }
+        usedWord.append(letter)
+    }
+    return true
+}
+
+
+isUnique(input: "Mohamed")
