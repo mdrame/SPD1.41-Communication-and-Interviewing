f7354c1078802932d5889402a76d0a078c95985a
mohammed <mdrame113@gmail.com>
2020-04-20 18:52:48 -0700

    question two

--- Exercism Problems + Test Cases.playground/Contents.swift
+++ Exercism Problems + Test Cases.playground/Contents.swift
@@ -1,11 +1,12 @@
 import UIKit
 import Foundation
 
+//Challenge 1: Are the letters unique?
 //Write a function that accepts a String as its only parameter, and returns true if the string has
 //only unique letters, taking letter case into account.
 
 func isUnique(input string: String)-> Bool  {
-    // 1. create a array of checked characters, then loop throught everry letter in the input string and append the latter of the list of checked characters, returning false as soon as a call to contain () fails.
+    //    create a array of checked characters, then loop throught everry letter in the input string and append the latter of the list of checked characters, returning false as soon as a call to contain () fails.
     var usedWord = [Character]()
     
     for letter in string {
@@ -24,9 +25,27 @@ assert(isUnique(input: "Adam") == true, "Sorry, string is not unique my friend")
 func refacterisUnique(input: String)->Bool {
     return Set(input).count == input.count
 }
-
+//
 print(refacterisUnique(input: "Dude"))
 assert(refacterisUnique(input: "Brother") == false, "Key is unique")
+
+
+
+//Challenge 2: Is a string a palindrome?
+// Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.
+
+func palindrome(input: String) -> Bool {
+// convert input to lowercased string of a new variable. check newly created lowercase array with input as an array as a condition statment. Retrun bool base of
+    let lowercase = input.lowercased()
+    //    print(lowercase)
+    if lowercase.reversed() == Array(lowercase) {
+        return true
+    }
+    return false
+}
+
+print(palindrome(input: "Never"))
+assert(palindrome(input: "Rotator") == true, "String not a palindrome")
 
 
 
